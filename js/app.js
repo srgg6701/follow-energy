35e441ad8c65a0a667b821ec2094c72c5846a7f1
sergei.klevtsov <sergei.klevtsov@accenture.com>
2017-12-09 11:55:25 +0200

    Adding pug template

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-(() => {
-    let menuContents = ``, lnk, text;
-    [
-        { "#.html": "What is energy indeed?" },
-        { "#.html": "How it will change my life?" },
-        { "#.html": "Why you cannot to obtain it and if it possible at all?" },
-        { "#.html": "Where does it reside and how to retrieve it?" },
-        { "#.html": "How to feel it?" }
-    ].forEach(link => {
-        lnk = Object.keys(link)[0];
-        menuContents += `                <li>
-        <a href="${lnk}">${link[lnk]}</a>
-    </li>`
-    });
-    $('ul.menu').html(menuContents);
-})();
-
 $(()=>{
     $('#hamburger').on('click', (event) => {
         $(event.target).next('.menu').fadeToggle();
